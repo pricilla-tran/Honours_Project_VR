dc5a548629bf7d997ceb11ade54cc18112d47178
pricilla-tran <71370147+pricilla-tran@users.noreply.github.com>
2022-10-16 13:34:26 +1100

    Big Commit!
    Hoping to get backgrounds and assets in tomorrow!

---
+++ Honours_VR/Assets/Scripts/PourDetector.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PourDetector : MonoBehaviour
+{
+    public int pourThreshold = 45;
+    public Transform origin = null;
+    public GameObject streamPrefab = null;
+
+    private bool isPouring = false;
+    private Stream currentStream = null;
+
+    private void Update()
+    {
+        bool pourCheck = CalculatePourAngle() < pourThreshold;
+        
+        if (isPouring != pourCheck)
+        {
+            isPouring = pourCheck;
+
+            if (isPouring)
+            {
+                StartPour();
+            }
+            else
+            {
+                EndPour();
+            }
+        }
+    }
+
+    private void EndPour()
+    {
+        print("End");
+        currentStream.End();
+    }
+
+    private void StartPour()
+    {
+        print("Start");
+        currentStream = CreateStream();
+        currentStream.Begin();
+    }
+
+    private float CalculatePourAngle()
+    {
+        return transform.forward.y * Mathf.Rad2Deg;
+    }
+
+    private Stream CreateStream()
+    {
+        GameObject streamObject = Instantiate(streamPrefab, origin.position, Quaternion.identity, transform);
+        return streamObject.GetComponent<Stream>();
+
+    }
+}
